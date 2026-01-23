72208e8a81f61c4b5b3fdfd989531a19c7c24100
dwh77 <dwh1998@vt.edu>
2026-01-23 18:53:52 -0500

    add packages through breakpoint script

--- Scripts/1_Install_Packages.R
+++ Scripts/1_Install_Packages.R
@@ -3,3 +3,25 @@
 install.packages('tidyverse')
 install.packages('plyr') #rbind.fill
 install.packages('dataRetrieval') #read in USGS data
+install.packages('ggpmisc') #stat_poly_line
+install.packages('mgcv') # for USGS to stage GAMs
+
+library(segmented) #for breakpoints
+
+#kruskal wallis boxplot function
+library(FSA)
+library(rcompanion)
+library(ggpubr)
+library(rlang)
+
+#arranging figures
+library(gridExtra)
+library(cowplot)
+library(patchwork)
+
+
+
+#from heatmaps
+# pacman::p_load(tidyverse, lubridate, akima, reshape2, 
+#                gridExtra, grid, colorRamps, RColorBrewer, cowplot)
+#for thermocline calcs; rLakeAnalyzer
